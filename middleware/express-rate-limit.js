f44cd30a04d925f8cc1b0ed2e312e3f929dd0510
Antho <matvienko.anthony@gmail.com>
2021-04-08 19:29:24 +0200

    last changes

--- middleware/express-rate-limit.js
+++ middleware/express-rate-limit.js
@@ -1,9 +1,9 @@
 const rateLimit = require("express-rate-limit");
 
-// === Limites de 5 requêtes toutes les 60 minutes pour éviter la Force Brute ===
+// === Limites de 100 requêtes toutes les 60 minutes pour éviter la Force Brute ===
 const apiLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 5
+  max: 100
 });
 
 module.exports = apiLimiter;
